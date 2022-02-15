d125458d93ad2ab28110d4f21a250d154d36555b
muzi <kaisenlee@gmail.com>
2022-02-16 03:08:09 +0800

    fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,6 @@ import { browser } from "$app/env";
   if (browser) import("virtual:windi-devtools")
 import SvelteTheme from 'svelte-themes/SvelteTheme.svelte'
 import PreloadingIndicator from "$lib/components/PreloadingIndicator.svelte";
-import ThemeToggler from "$lib/components/ThemeToggler.svelte";
 import Main from "$lib/components/Main.svelte";
 import TheHeader from "$lib/components/TheHeader.svelte";
 import TheFooter from "$lib/components/TheFooter.svelte";
